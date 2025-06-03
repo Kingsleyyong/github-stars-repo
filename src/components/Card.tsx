7b59a59cd7ad8b6072f894800cb3fa38d6894653
Kingsley Yong <kingsleyyong0208@gmail.com>
2025-06-03 16:11:16 +0800

    Header

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+import type { RepoDetailTypes } from "../types/commonTypes"
+
+interface CardProps {
+      repoDetails: RepoDetailTypes
+}
+
+export const Card = ({repoDetails}: CardProps) => {
+  return (
+    <div>
+      <h3></h3>
+    </div>
+  )
+}
---
+++ src/types/commonTypes.ts
@@ -0,0 +1,5 @@
+export type RepoDetailTypes = {
+      id: number;
+      name: string;
+      description: string;
+}
